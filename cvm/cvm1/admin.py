1778d4c059a1d96c7c610977bab9bc139e6ed289
Rafał Selewońko <rafal@selewonko.com>
2011-11-23 09:54:03 +0100

    Fixed whitespaces

--- cvm/cvm1/admin.py
+++ cvm/cvm1/admin.py
@@ -2,21 +2,21 @@ from .models import *
 from django.contrib import admin
 
 class SectionEntryAdminInline(admin.TabularInline):
-	model = SectionEntry
+    model = SectionEntry
 
 class SectionAdmin(admin.ModelAdmin):
-	list_select_related = True
-	inlines = [SectionEntryAdminInline, ]
+    list_select_related = True
+    inlines = [SectionEntryAdminInline, ]
 
 class ResumeAdmin(admin.ModelAdmin):
-	list_filter = ('active', 'public')
-	list_select_related = True
+    list_filter = ('active', 'public')
+    list_select_related = True
 
 class TemplateAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 class TemplateVariantAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('name',)}
+    prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Resume, ResumeAdmin)
 admin.site.register(Section, SectionAdmin)
--- cvm/cvm1/models.py
+++ cvm/cvm1/models.py
@@ -2,76 +2,81 @@
 
 import datetime
 
-from django.db import models
 from django.contrib.auth.models import User
-from django.utils.translation import ugettext_lazy as _
+from django.db import models
 from django.template.defaultfilters import slugify
+from django.utils.translation import ugettext_lazy as _
+
 
 class Template(models.Model):
-	name = models.CharField(max_length=100)
-	slug = models.SlugField() #'templates/'+slugify(name)+'/'
+    name = models.CharField(max_length=256, verbose_name=_(u'name'))
+    slug = models.SlugField(max_length=64, verbose_name=_(u'slug'))
+    
+    class Meta:
+        verbose_name = _(u'template')
+        verbose_name_plural = _(u'templates')
 
-	class Meta:
-		verbose_name = _(u'template')
-		verbose_name_plural = _(u'templates')
-
-	def __unicode__(self):
-		return self.name
+    def __unicode__(self):
+        return self.name
 
 class TemplateVariant(models.Model):
-	template = models.ForeignKey(Template)
-	name = models.CharField(max_length=100)
-	slug = models.SlugField() #'templates/'+slugify(template.name)+'/'+slugify(name)+'/'
+    template = models.ForeignKey('Template', verbose_name=_('template'))
+    name = models.CharField(max_length=256, verbose_name=_('name'))
+    slug = models.SlugField(max_length=256, verbose_name=_('name'))
 
-	class Meta:
-		verbose_name = _(u'variant')
-		verbose_name_plural = _(u'variants')
 
-	def __unicode__(self):
-		return '%s/%s' % (self.template.name, self.name)
+    class Meta:
+        verbose_name = _(u'variant')
+        verbose_name_plural = _(u'variants')
+
+    def __unicode__(self):
+        return '%s/%s' % (self.template.name, self.name)
+
 
 class Resume(models.Model):
-	# scrapped User field, since Identity is linked to User
+    identity = models.ForeignKey('accounts.Identity')
+    name = models.CharField(verbose_name=_(u'resume_name'), max_length=50)
+    creation_date = models.DateTimeField(verbose_name=_(u'creation_date'), auto_now_add=True)
+    mod_date = models.DateTimeField(verbose_name=_(u'mod_date'), auto_now=True)
+    template_variant = models.ForeignKey(TemplateVariant)
+    active = models.BooleanField()
+    public = models.BooleanField()
 
-	identity = models.ForeignKey('accounts.Identity')
-	name = models.CharField(verbose_name=_(u'resume_name'), max_length=50)
-	creation_date = models.DateTimeField(verbose_name=_(u'creation_date'), auto_now_add=True)
-	mod_date = models.DateTimeField(verbose_name=_(u'mod_date'), auto_now=True)
-	template_variant = models.ForeignKey(TemplateVariant)
-	active = models.BooleanField()
-	public = models.BooleanField()
 
-	class Meta:
-		verbose_name = _(u'resume')
-		verbose_name_plural = _(u'resumes')
+    class Meta:
+        verbose_name = _(u'resume')
+        verbose_name_plural = _(u'resumes')
 
-	def __unicode__(self):
-		return '%s/%s' % (self.identity.system_name, self.name)
+    def __unicode__(self):
+        return '%s/%s' % (self.identity.system_name, self.name)
+
 
 class Section(models.Model):
-	SEC_TYPES = list(enumerate([u'', u'wypunktowana', u'datowana', u'słownikowa']))
+    SEC_TYPES = list(enumerate([u'', u'wypunktowana', u'datowana', u'słownikowa']))  # TODO: ustawic stale zamiast enumerte
 
-	resume = models.ForeignKey(Resume)
-	title = models.CharField(verbose_name=_(u'section_title'), max_length=100)
-	sec_type = models.PositiveSmallIntegerField(verbose_name=_(u'section_type'), choices=SEC_TYPES)
+    resume = models.ForeignKey('Resume')
+    title = models.CharField(verbose_name=_(u'section_title'), max_length=100)
+    sec_type = models.PositiveSmallIntegerField(verbose_name=_(u'section_type'), choices=SEC_TYPES)
 
-	class Meta:
-		verbose_name = _(u'section')
-		verbose_name_plural = _(u'sections')
-	
-	def __unicode__(self):
-		return '%s/%s' % (self.resume, self.title)
+    class Meta:
+        verbose_name = _(u'section')
+        verbose_name_plural = _(u'sections')
+
+    def __unicode__(self):
+        return '%s/%s' % (self.resume, self.title)
+
 
 class SectionEntry(models.Model):
-	section = models.ForeignKey(Section)
-	from_date = models.DateField(verbose_name=_(u'from_date'), blank=True) # datowana
-	to_date = models.DateField(verbose_name=_(u'to_date'), blank=True) # datowana
-	title = models.CharField(verbose_name=_(u'entry_title'), blank=True, max_length=100) # słownikowa
-	content = models.CharField(verbose_name=_(u'entry_content'), max_length=255)
+    section = models.ForeignKey(Section)	
+    from_date = models.DateField(verbose_name=_(u'from_date'), blank=True) # datowana
+    to_date = models.DateField(verbose_name=_(u'to_date'), blank=True) # datowana
+    title = models.CharField(verbose_name=_(u'entry_title'), blank=True, max_length=100) # słownikowa
+    content = models.CharField(verbose_name=_(u'entry_content'), max_length=255)
 
-	class Meta:
-		verbose_name = _(u'entry')
-		verbose_name_plural = _(u'entries')
+    class Meta:
+        verbose_name = _(u'entry')
+        verbose_name_plural = _(u'entries')
 
-	def __unicode__(self):
-		return '%s/%s' % (self.section, self.title)
+    def __unicode__(self):
+        return '%s/%s' % (self.section, self.title)
+
